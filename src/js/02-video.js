38c1302517d73f1d14eaf79d697c456eddcab87f
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-22 11:05:21 +0100

    end 2.6

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,11 @@ import Player from "@vimeo/player";
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
-player.on("play", function () {
-	console.log("played the video!");
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+player.on("timeupdate", function () {
+	player.getCurrentTime().then(function (seconds) {
+		console.log(seconds);
+		localStorage.setItem("videoplayer-current-time", seconds);
+	});
 });
